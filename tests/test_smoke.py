e4a886ee86caa339d7a482cfe506c78c4783e9e9
segurastward <stwardsegura239@gmail.com>
2026-09-23 10:39:55 -0600

    test: add package import smoke test

---
+++ tests/test_smoke.py
@@ -0,0 +1,12 @@
+
+
+def test_something():
+    assert 1 +1 == 2
+
+
+def test_structure():
+    import domain,application, infrastructure
+
+    assert domain
+    assert application
+    assert infrastructure
